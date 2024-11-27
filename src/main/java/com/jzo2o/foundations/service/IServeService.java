c0a5e18dee7ab91920279a02b0c503a3de64ec28
Catking <sypbrgb5764@gmail.com>
2024-11-27 20:02:25 +0800

    区域服务查询

---
+++ src/main/java/com/jzo2o/foundations/service/IServeService.java
@@ -0,0 +1,24 @@
+package com.jzo2o.foundations.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.jzo2o.common.model.PageResult;
+import com.jzo2o.foundations.model.domain.Serve;
+import com.jzo2o.foundations.model.dto.request.ServePageQueryReqDTO;
+import com.jzo2o.foundations.model.dto.response.ServeResDTO;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @author itcast
+ */
+public interface IServeService extends IService<Serve> {
+
+    /**
+     * 分页查询服务列表
+     * @param servePageQueryReqDTO 查询条件
+     * @return 分页结果
+     */
+    PageResult<ServeResDTO> page(ServePageQueryReqDTO servePageQueryReqDTO);
+}
---
+++ src/main/java/com/jzo2o/foundations/service/impl/ServeServiceImpl.java
@@ -0,0 +1,40 @@
+package com.jzo2o.foundations.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jzo2o.common.model.PageResult;
+import com.jzo2o.foundations.mapper.ServeMapper;
+import com.jzo2o.foundations.model.domain.Serve;
+import com.jzo2o.foundations.model.dto.request.ServePageQueryReqDTO;
+import com.jzo2o.foundations.model.dto.response.ServeResDTO;
+import com.jzo2o.foundations.service.IServeService;
+import com.jzo2o.mysql.utils.PageHelperUtils;
+import com.jzo2o.mysql.utils.PageUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author itcast
+ */
+@Service
+public class ServeServiceImpl extends ServiceImpl<ServeMapper, Serve> implements IServeService {
+
+    /**
+     * 分页查询
+     *
+     * @param servePageQueryReqDTO 查询条件
+     * @return 分页结果
+     */
+    @Override
+    public PageResult<ServeResDTO> page(ServePageQueryReqDTO servePageQueryReqDTO) {
+        //通过baseMapper调用queryServeListByRegionId方法
+        //调用mapper查询数据，这里由于继承了ServiceImpl<ServeMapper, Serve>，使用baseMapper相当于使用ServeMapper
+        PageResult<ServeResDTO> serveResDTOPageResult = PageHelperUtils
+                .selectPage(servePageQueryReqDTO, () -> baseMapper.queryServeListByRegionId(servePageQueryReqDTO.getRegionId()));
+        return serveResDTOPageResult;
+    }
+}
